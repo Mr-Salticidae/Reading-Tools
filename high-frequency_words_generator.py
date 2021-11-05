515a4e46c2d2e62c5df588df34e30122b8d9f176
Chu WANG <wc1225827513@gmail.com>
2021-11-05 10:51:56 +0800

    Update the cleaning process and Tokenize the corpus

--- high-frequency_words_generator.py
+++ high-frequency_words_generator.py
@@ -1,5 +1,6 @@
 import os
 import docx2txt
+from nltk.tokenize import word_tokenize, RegexpTokenizer
 
 SOURCES_FOLDER_PATH = './sources'
 RESULTS_FOLDER_PATH = './results'
@@ -30,13 +31,34 @@ def read_all_files(file_list):
 def clean_dataset(dataset):
     corpus = []
     for article in dataset:
+        # Get rid of bold
         article = article.replace(u'\xa0', u' ')
+
+        # Get rid of \n and the space
         article = article.replace(u'\n', u' ')
         article = article.split(r"\n|[' ']")
-        corpus.append(article[0])
+        article = article[0]
+
+        # Lower every word
+        article = article.lower()
+
+        corpus.append(article)
     return corpus
+
+# Tokenize the corpus
+
+
+def tokenize_corpus(corpus):
+    tokens = []
+    tokenizer = RegexpTokenizer(r'\w+')
+    for article in corpus:
+        tokens_per_article = tokenizer.tokenize(article)
+        for token in tokens_per_article:
+            tokens.append(token)
+    return tokens
 
 
 file_list = get_all_files(SOURCES_FOLDER_PATH)
 dataset = read_all_files(file_list)
 corpus = clean_dataset(dataset)
+tokens = tokenize_corpus(corpus)
